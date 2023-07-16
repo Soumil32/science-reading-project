7ef3a9562755d42e1af1f0807f1af6b41bdec5e8
Soumil <soumil009@outlook.com>
2023-07-16 16:46:27 +0100

    done with week 3

--- src/routes/week-3/+page.svelte
+++ src/routes/week-3/+page.svelte
@@ -1,5 +1,22 @@
-<main class="text-center">
-    <h1>Learning From Other People's Mistakes</h1>
+<main class="flex flex-col items-center gap-10 m-16">
+    <h1 class="text-2xl font-bold">Learning From Other People's Mistakes</h1>
 
-    <p></p>
+    <p>Chris Hadfield emphasises the importance of learning from you rmistakes instead of being shamed of them. Mistakes can be very helpful in avoiding futute repeats of past errors. Lets talk about intaces Hadfield has mentioned:</p>
+    <h2 class="text-xl font-bold">Pilots</h2>
+    <p>
+        Chris Hadfield used to work as a army pilot and test pilot. During his years as a pilot he has seen many pilots make mistakes. He has seen pilots make mistakes that have cost them their lives. He talks about meetings
+        pilots regularly have where they discuss mistakes they have made and how they can be avoided in the future. This is absolutely crucial as it avoids injuries and dmages that could have been otherwise avoided.
+        In the meetings, every pilot shows everything they have done in the past week including all major and minor mistakes they have made. Meanwhile, everyone else in the room is not expected to judge the pilot
+        and instead to be listening out for things that can be done in the future to avoid others making the same mistakes. One example Hadfield talks of is a pilot's plane slipping off the end of the runway in Louisiana. 
+        The plane only had light damages so the pilot could have kept his mouth shut and no one would have known. However, he decided to tell everyone about it so that others knew that the Louisiana runaway made made of a special 
+        type of asphalt which included seashells leading to runway getting slippery when wet. Him sharing this information could have saving countless accidents happening in the future.
+    </p>
+    <h2 class="text-xl font-bold">Astronauts and The Creation of 'Flight Rules'</h2>
+    <p>
+        Similarly to pilots, astronauts encounter many mistakes while in simulations and in space. However, unlike pilots, astronauts have a much smaller margin for error. Even the smallest oversite can lead to a disaster.
+        Just like in the military pilots, astronauts have meetings where they discuss mistakes they have made and how they can be avoided in the future. Everyone involved in the mission is expected to attend these meetings.
+        Here, every little thing astronauts do is put under a microscope and analysed  with heavy critisism. This is extremely nessesary as it allows for flaws to be found on Earth, where they can solved, instead of in space where
+        they could be lethal. Since the 1960's  all the mistakes made by astronauts have been recorded with verbosely detailed solutions that can be reffered to in the future as the single source of truth. These are called 'Flight Rules'.
+        'Flight Rules' are followed strictly by all astronauts and are updated regularly. They are also used by NASA to train new astronauts. This is a great example of how mistakes can be used to improve the future.
+    </p>
 </main>
