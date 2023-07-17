9830e0346414ce7abc514f853089d75ab88ec577
Soumil <soumil009@outlook.com>
2023-07-18 00:18:27 +0100

    week 5 complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <nav>
-    <a href="/week-1">Week 1</a><br><a href="/week-2">Week 2</a><br><a href="/week-3">Week 3</a><br><a href="/week-4">Week 4</a><br><a href="/week-5">Week 5</a><br><a href="/week-6">Week 6</a><br><a href="/week-7">Week 7</a><br>
+    <a href="/week-1">Week 1</a><br><a href="/week-2">Week 2</a><br><a href="/week-3">Week 3</a><br><a href="/week-4">Week 4</a><br><a href="/week-5">Week 5</a><br><a href="/week-6">Week 6</a><br>
 </nav>
---
+++ src/routes/week-5/+page.svelte
@@ -0,0 +1,19 @@
+<h1>The International Space Station</h1>
+
+<p>The Internation Space Station (ISS) is structure built by many contries working together. Atronauts stay here doing their missions and conduct scientific experiments which are vital in creating inventions to help on Earth</p>
+
+<h2 class="text-xl font-bold">Living on the ISS</h2>
+
+<p>Life onboard the International Space Station (ISS) presents a stark contrast to life on Earth, primarily due to the absence of gravity in space. This lack of gravity significantly impacts everyday activities that we take for granted on Earth, such as running water, showers, and traditional toilets. As a result, astronauts rely on specialized equipment and ingenious solutions to perform these essential tasks.</p>
+
+    <p>For instance, the absence of running water means that conventional showers are not feasible on the ISS. Instead, astronauts use specially formulated "no-rinse" shampoo that they apply to their scalps. After massaging the shampoo into their hair, they use a towel to wipe it off, leaving their hair clean and refreshed.</p>
+
+    <p>Brushing teeth in microgravity also requires adaptation. Astronauts apply toothpaste onto a toothbrush and carefully brush their teeth, but spitting the toothpaste into a sink is not possible without gravity. Instead, they have to swallow the toothpaste, which might seem peculiar, but it is a necessary workaround in the weightless environment of the ISS.</p>
+
+    <p>To counteract the detrimental effects of prolonged weightlessness on their muscles and bones, astronauts dedicate two hours each day to exercise. This rigorous workout routine is vital to maintain their physical health during their stay in space. Without regular exercise, the lack of resistance and gravity can cause muscles to weaken and bones to become brittle.</p>
+
+    <p>During their exercise sessions, astronauts are strapped to specialized treadmills or exercise bikes to keep them anchored to the equipment. This prevents them from floating around aimlessly while they work out. Additionally, they wear towels under their clothes to absorb sweat since, in the absence of gravity, sweat does not flow down the body. The towel serves a dual purpose as it is later left to dry so that the sweat can be recycled into clean water, along with urine and other wastewater on the ISS.</p>
+
+    <p>Recycling water is a crucial practice on the ISS, as water is a limited resource in space. By reusing and purifying water from various sources, including sweat and urine, astronauts ensure a sustainable supply of clean water for their daily needs.</p>
+
+    <p>These adaptations and innovative solutions demonstrate the resourcefulness and adaptability of astronauts in the challenging environment of space. The ISS serves as a unique laboratory for learning how to thrive in microgravity, and these lessons are vital for planning future long-duration space missions, such as missions to Mars. Through constant innovation and problem-solving, astronauts continue to push the boundaries of human exploration and inspire generations to come.</p>
