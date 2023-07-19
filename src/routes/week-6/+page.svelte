71af3997cf56f76b585f85bb56dbfedfb2aa45ea
Soumil <soumil009@outlook.com>
2023-07-19 23:12:39 +0100

    done week 6

---
+++ src/routes/week-6/+page.svelte
@@ -0,0 +1,21 @@
+<h1 class="text-2xl font-bold">Making an Effective Re-Entry to Earth</h1>
+
+<p>Re-entering Earth can be extremely difficult from the physical aspects of entering the atmosphere to well-being of the astronauts.</p>
+
+<h2 class="text-xl font-bold">Entering the Atmosphere</h2>
+
+<p>When astronauts return to Earth from their space missions, they rely on the same Soyuz capsule that transported them to space. The Soyuz capsule consists of three distinct sections: the orbital module, the recovery module, and the service module, each serving a specific purpose in the journey.</p>
+
+<p>The orbital module is where the astronauts reside during the period between leaving Earth and reaching the International Space Station (ISS). This module provides a living area and serves as their temporary home while in space, offering them a place to eat, sleep, and carry out necessary activities during their space travel.</p>
+
+<p>During takeoff and landing, the astronauts occupy the recovery module of the Soyuz capsule. This module is specifically designed to accommodate the astronauts and ensure their safety during these critical phases of the mission. It provides seating and restraints to secure the astronauts in place, protecting them during the dynamic and potentially turbulent process of launch and re-entry.</p>
+
+<p>The service module, on the other hand, houses the spacecraft's fuel and engines. It plays a crucial role in providing the necessary propulsion and support systems for the Soyuz capsule to maneuver in space and carry out its mission objectives.</p>
+
+<p>During re-entry into Earth's atmosphere, only the recovery module of the Soyuz capsule returns to the surface. This module is equipped with a heat shield to protect the astronauts from the intense heat generated during the re-entry process. The heat shield effectively absorbs and dissipates the heat, safeguarding the astronauts inside.</p>
+
+<p>Soyuz capsule landings are renowned for their rough nature compared to other spacecraft, such as the Space Shuttle. The Soyuz capsule descends at a faster speed and has a smaller landing area, often resulting in a bumpy touchdown. The Soyuz capsules typically land in the remote desert region of Kazakhstan.</p>
+
+<p>The re-entry experience can be physically demanding for the returning astronauts. As the Soyuz capsule descends through Earth's atmosphere, the forces experienced by the astronauts increase significantly. During this period, they are subjected to extremely high g-forces, which can be around four times the gravity experienced on Earth. These forces, combined with the abrupt transition from a weightless environment to the pull of Earth's gravity, can result in astronauts feeling sore and physically strained upon landing.</p>
+
+<p>Despite the challenges and physical discomfort, the Soyuz capsule has proven to be a reliable and efficient means of transporting astronauts to and from space. It has played a vital role in numerous missions and continues to be a cornerstone of human spaceflight. The experiences and knowledge gained from Soyuz landings contribute to ongoing advancements in space travel, ensuring the safety and well-being of astronauts as they embark on their incredible journeys beyond Earth's atmosphere.</p>
